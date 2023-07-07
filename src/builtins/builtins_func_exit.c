5f0ab9da782530fdecabe8de32dacbd96a62e20f
Victor Chizhov <victor.chizhov93@gmail.com>
2023-07-07 13:23:54 +0400

    exit ok cd "-" ok fix echo fix history fix leaks

--- src/builtins/builtins_func_exit.c
+++ src/builtins/builtins_func_exit.c
@@ -6,11 +6,51 @@
 /*   By: vchizhov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 16:40:45 by vchizhov          #+#    #+#             */
-/*   Updated: 2023/07/04 15:12:32 by vchizhov         ###   ########.fr       */
+/*   Updated: 2023/07/07 13:21:20 by vchizhov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+int	ft_check_longlong(char *str)
+{
+	int					i;
+	unsigned long long	res;
+
+	i = 0;
+	res = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+		if (res > LONG_MAX)
+			return (1);
+	}
+	return (0);
+}
+
+int	check(char *str)
+{
+	if (!ft_strcmp(str, "-9223372036854775808"))
+		return (0);
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str && *str == '0')
+		str++;
+	if (ft_check_longlong(str) || ft_strlen(str) >= 20)
+		return (1);
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (1);
+		str++;
+	}
+	return (0);
+}
 
 void	print_error(t_parser_token **token_key, int index)
 {
@@ -53,7 +93,7 @@ void	exit_func(t_array_list *line, t_shell *shell)
 	token_key = (t_parser_token **)line->array;
 	index = 1;
 	g_err_no = 0;
-	if (check_number_arg(token_key, shell))
+	if (check_number_arg(token_key, shell) && check(token_key[1]->content) == 0)
 	{
 		if (line->size <= 2)
 		{
